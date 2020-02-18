86fa52eb7cf03b57b4b979a4d9480c887bae9fb1
wongRegina <wong.regina.1@gmail.com>
2020-02-18 11:03:22 -0500

    First two parts of the homework done. The buttons on the home page work

---
+++ src/gologolo/GoLogoLoController.js
@@ -0,0 +1,43 @@
+import AppsterController from '../appster/AppsterController.js'
+import {AppsterGUIId, AppsterHTML} from '../appster/AppsterConstants.js'
+import {GoLogoLoCallBack} from './GoLogoLoConstants.js'
+import GoLogoLoLogo from './GoLogoLoLogo.js'
+
+export default class GoLogoLoController
+ extends AppsterController {
+    constructor() {
+        super();
+        
+    }
+
+    processEditText() {
+        this.model.updateText();
+    }
+
+    registerAppsterEventHandlers(){
+        super.registerAppsterEventHandlers();
+
+        // Enter and Cancel Button
+        this.registerEventHandler(AppsterGUIId.APPSTER_TEXT_INPUT_MODAL_ENTER_BUTTON, AppsterHTML.CLICK, this[GoLogoLoCallBack.GOLOGOGO_PROCESS_ENTER_CREATE_WORK]);
+        this.registerEventHandler(AppsterGUIId.APPSTER_TEXT_INPUT_MODAL_CANCEL_BUTTON, AppsterHTML.CLICK, this[GoLogoLoCallBack.GOLOGOGO_PROCESS_CANCEL_CREATE_WORK]);
+    }
+
+    processEnterCreateWork = () =>{
+        console.log("processEnterCreateWork");
+        let dialog = document.getElementById(AppsterGUIId.APPSTER_TEXT_INPUT_MODAL_TEXTFIELD).value;
+        if(dialog.length < 1){
+
+        }
+        else if(this.model.getRecentWork(dialog) != null){
+            this.model.view.showErrorForDuplicateText();
+        }
+        else{
+            this.model.prependWork(this.model.createNewWork(dialog));
+        }
+    }
+    
+    processCancelCreateWork = () => {
+        console.log("processCancelCreateWork");
+        this.model.view.processCancelButton();
+    }
+}
